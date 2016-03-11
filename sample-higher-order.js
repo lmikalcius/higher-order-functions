26c01848bff999396a2822464d5aa23570da497d
Linas Mikalcius <linas.mikalcius@gmail.com>
2016-03-11 15:05:13 -0600

    Fix indentation

--- sample-higher-order.js
+++ sample-higher-order.js
@@ -1,28 +1,28 @@
 /* function noisy(func) {
-	return function(arg) {
-  	console.log("calling function with", arg.toString());
+  return function(arg) {
+    console.log("calling function with", arg.toString());
     var valueReturned = func(arg);
     console.log("called function and got", valueReturned.toString());
   }
 }
 
 var square = function(num) { 
-	return num *= num; 
+  return num *= num; 
 };
 
 noisy(square)(4); */
 
 
 function noisyMultipleArgs(func) {
-	return function(args) {
-  	console.log("calling function with", args.toString());
+  return function(args) {
+    console.log("calling function with", args.toString());
     var valueReturned = func.apply(null, args);
     console.log("called function and got", valueReturned.toString());
   }
 }
 
 function add(num1, num2) {
-	return num1 + num2;
+  return num1 + num2;
 }
 
 noisyMultipleArgs(add)([1, 2]);
@@ -30,29 +30,29 @@ noisyMultipleArgs(add)([1, 2]);
 
 
 /* function unless(test, then) {
-	if (!test) then();
+  if (!test) then();
 }
 
 for (var i = 0; i < 5; i++) {
-	unless(i % 2 === 0, function() {
-  	console.log(i.toString(), "is not even");
+  unless(i % 2 === 0, function() {
+    console.log(i.toString(), "is not even");
   });
 } 
 
 function repeat(times, body) {
-	for (var i = 1; i <= times; i++) {
-  	body(i);
+  for (var i = 1; i <= times; i++) {
+    body(i);
   }
 }
 
 
 
 repeat(4, function(num) {
-	console.log(num * 10);
+  console.log(num * 10);
 })
 
 repeat(6, function(i) {
-	unless(i % 2, function() {
-  	console.log(i.toString(), "is even")
+  unless(i % 2, function() {
+    console.log(i.toString(), "is even")
   })
 }); */
